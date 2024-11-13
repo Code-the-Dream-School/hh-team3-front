bf0cadffa8fc7142817651efab32a9d6d52b9744
olgakahraman <ol.khrmn@gmail.com>
2024-11-13 11:59:20 -0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import { getAllData } from './util/index';
 import BookCardMain from '/src/components/BookCard/BookCardMain.jsx';
 import booksData from '/src/data/booksData.js';
 
+import BookDetailsMain from '/src/components/BookDetails/BookDetailsMain.jsx';
+
 const URL = 'http://localhost:8000/api/v1/';
 
 function App() {
@@ -23,11 +25,15 @@ function App() {
 		return <BookCardMain key={item.id} {...item} />;
 	});
 
+	const books = booksData.map((item) => {
+		return <BookDetailsMain key={item.id} {...item} />;
+	});
+
 	return (
 		<div className="container">
-			{/* <Navbar /> */}
-		
+			<h1>{message}</h1>
 			<section className="cards-list">{cards}</section>
+			<section className="books-page">{books}</section>
 		</div>
 	);
 }
