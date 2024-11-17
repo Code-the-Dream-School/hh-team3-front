0f24c02af105df7c5f71392fad48c2e57513abde
olgakahraman <117988074+olgakahraman@users.noreply.github.com>
2024-11-16 21:16:34 -0500

    BookCard folder (#1)
    Added BookCard component with basic structure and styles
    
    - Created a BookCard folder with BookCardMain, BookCardHeader, and BookCardBody components.
    - Added sample data in booksData.js for testing before API integration.
    - Implemented basic CSS modules for styling; design will be improved later.
    - Passed props to BookCard components and displayed a list of books in App.jsx.
    - Added support for cover images and destructured props for cleaner code.

---
+++ src/data/booksData.js
@@ -0,0 +1,44 @@
+export default [
+	{
+		id: 1,
+		title: 'Pride and Prejustice',
+		author: 'Jane Austen',
+		description:
+			'The romantic clash of two opinionated young people provides the sustaining theme of Pride and Prejudice. Vivacious Elizabeth Bennet is fascinated and repelled by the arrogant Mr. Darcy, whose condescending airs and acrid tongue have alienated her entire family. Their spirited courtship is conducted against a back- ground of assembly-ball flirtations and draw- ing-room intrioues.',
+		publishDate: '1961',
+		quote:
+			'It is a truth universally acknowledged of a good fortune, must be in want of a wife.',
+		coverImg: 'img-1.jpeg',
+		stats: {
+			rating: 5.0,
+		},
+	},
+	{
+		id: 2,
+		title: 'Jane Eyre',
+		author: 'Charlotte Bronte',
+		description:
+			"A gothic masterpiece of tempestuous passions and dark secrets, Charlotte Bronte's Jane Eyre is edited with an introduction and notes by Stevie Davis in Penguin Classics. Charlotte Bronte tells the story of orphaned Jane Eyre, who grows up in the home of her heartless aunt, enduring loneliness and cruelty.",
+		publishDate: '2011',
+		quote:
+			'The human beings gathered round me appeared to me not realities but shadows.',
+		coverImg: 'img-2.jpeg',
+		stats: {
+			rating: 5.0,
+		},
+	},
+	{
+		id: 3,
+		title: '1984',
+		author: 'George Orwell',
+		description:
+			'1984 is a dystopian novel that was written by George Orwell and published in 1949. It tells the story of Winston Smith, a citizen of the miserable society of Oceania, who is trying to rebel against the Party and its omnipresent symbol, Big Brother.',
+		publishDate: '2013',
+		quote:
+			'Perhaps one did not want to be loved so much as to be understood.',
+		coverImg: 'img-3.jpeg',
+		stats: {
+			rating: 5.0,
+		},
+	},
+];
