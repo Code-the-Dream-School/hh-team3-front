669e8ed71b249e86320ea6272f22d44d2cb067e7
betymesfin <98676637+betymesfin@users.noreply.github.com>
2024-11-19 22:37:07 -0600

    added navbar component (#2)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/NavBar/Navbar'
 import BookCard from '/src/components/BookCard/BookCard.jsx';
 import BookDetails from '/src/components/BookDetails/BookDetails.jsx';
 import booksData from '/src/data/booksData.js';
-
 const URL = 'http://localhost:8000/api/v1/';
 
 function App() {
-	const cards = booksData.map((item) => {
+const cards = booksData.map((item) => {
 		return <BookCard key={item.id} {...item} />;
 	});
 
@@ -14,12 +14,17 @@ function App() {
 		return <BookDetails key={item.id} {...item} />;
 	});
 
-	return (
-		<div className="container">
+  return (
+    <>
+    <BrowserRouter>
+       <Navbar />
+    </BrowserRouter>
+    <div className="container">
 			<section className="cards-list">{cards}</section>
 			<section className="books-page">{books}</section>
 		</div>
-	);
+      
+    </>
+  );
 }
-
 export default App;
---
+++ src/components/NavBar/Navbar.jsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './Navbar.css'; 
+
+function Navbar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-dark custom-navbar">
+      <div className="container">
+        <Link className="navbar-brand" to="/">BookTalk</Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNav">
+          <ul className="navbar-nav ms-auto">
+            <li className="nav-item">
+              <Link className="nav-link" to="/find-book">Find a book</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/find-discussion">Find a discussion</Link>
+            </li>
+            <li className="nav-item">
+              <Link className="nav-link" to="/about">About Us</Link>
+            </li>
+          </ul>
+          <div className="d-flex ms-3">
+            <Link className="btn btn-outline-light custom-btn me-2" to="/signup">Sign up</Link>
+            <Link className="btn btn-outline-light custom-btn" to="/login">Log in</Link>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
