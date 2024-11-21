96167546fdc9c0eec6e205f8b72d15535cc39678
betymesfin <98676637+betymesfin@users.noreply.github.com>
2024-11-20 22:53:39 -0600

    formatted

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,27 @@
-import { BrowserRouter , Routes,Route} from 'react-router-dom';
-import Navbar from './components/NavBar/Navbar'
-import BookCard from '/src/components/BookCard/BookCard.jsx';
-import BookDetails from '/src/components/BookDetails/BookDetails.jsx';
-import booksData from '/src/data/booksData.js';
-const URL = 'http://localhost:8000/api/v1/';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/NavBar/Navbar";
+import BookCard from "/src/components/BookCard/BookCard.jsx";
+import BookDetails from "/src/components/BookDetails/BookDetails.jsx";
+import booksData from "/src/data/booksData.js";
+const URL = "http://localhost:8000/api/v1/";
 
 function App() {
-
-	const books = booksData.map((item) => {
-		return <BookDetails key={item.id} {...item} />;
-	});
+  const books = booksData.map((item) => {
+    return <BookDetails key={item.id} {...item} />;
+  });
 
   return (
-	<>
-    <BrowserRouter>
-       <Navbar />
-       <Routes>
-        <Route path="/find-book" element={<BookCard booksData={booksData}/>}/>
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route
+            path="/find-book"
+            element={<BookCard booksData={booksData} />}
+          />
         </Routes>
-    </BrowserRouter>  
+      </BrowserRouter>
     </>
-
   );
 }
 export default App;
--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,22 +1,20 @@
-import React from 'react';
-import './BookCard.css';
+import React from "react";
+import "./BookCard.css";
 
-export default function BookCard({booksData}) {
-	return (
-		<div className="day-theme">
-			{booksData.map((item) => (
-			<div key={item.id} className="card">
-                <img src={`../images/${item.coverImg}`} />
-                <div className="card-content">
-                    <p className="card-title">{item.title}</p>
-                    <p className="card-author">
-                    By {item.author} ({item.publishDate})
-                </p>
-                
-                </div>
-            </div>
-        ))}
+export default function BookCard({ booksData }) {
+  return (
+    <div className="day-theme">
+      {booksData.map((item) => (
+        <div key={item.id} className="card">
+          <img src={`../images/${item.coverImg}`} />
+          <div className="card-content">
+            <p className="card-title">{item.title}</p>
+            <p className="card-author">
+              By {item.author} ({item.publishDate})
+            </p>
+          </div>
+        </div>
+      ))}
     </div>
-
-	);
+  );
 }
