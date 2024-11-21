639c8a6b777465b98bf187fab2c83d4600e57dda
betymesfin <98676637+betymesfin@users.noreply.github.com>
2024-11-20 22:56:55 -0600

    combined bookcard and nav bar (#6)
    * combined bookcard and nav bar
    
    * formatted

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,21 +1,20 @@
-import React from 'react';
-import './BookCard.css';
+import React from "react";
+import "./BookCard.css";
 
-export default function BookCard({
-	coverImg,
-	authors,
-	title,
-	publishDate,
-}) {
-	return (
-		<div className="card day-theme">
-			<img src={`../images/${coverImg}`} />
-			<div className="card-content">
-				<p className="card-title"> {title}</p>
-				<p className="card-author">
-					By {authors} ({publishDate})
-				</p>
-			</div>
-		</div>
-	);
+export default function BookCard({ booksData }) {
+  return (
+    <div className="day-theme">
+      {booksData.map((item) => (
+        <div key={item.id} className="card">
+          <img src={`../images/${item.coverImg}`} />
+          <div className="card-content">
+            <p className="card-title">{item.title}</p>
+            <p className="card-author">
+              By {item.author} ({item.publishDate})
+            </p>
+          </div>
+        </div>
+      ))}
+    </div>
+  );
 }
--- src/data/booksData.js
+++ src/data/booksData.js
@@ -1,113 +1,127 @@
 export default [
-	{
-		id: 1,
-		title: 'Pride and Prejustice',
-		authors: ['Jane Austen'],
-		categories: ['Fiction'],
-		description:
-			'The romantic clash of two opinionated young people provides the sustaining theme of Pride and Prejudice. Vivacious Elizabeth Bennet is fascinated and repelled by the arrogant Mr. Darcy, whose condescending airs and acrid tongue have alienated her entire family. Their spirited courtship is conducted against a back- ground of assembly-ball flirtations and draw- ing-room intrioues.',
-		publishDate: '1961',
+  {
+    id: 1,
+    title: "Pride and Prejustice",
+    authors: ["Jane Austen"],
+    categories: ["Fiction"],
+    description:
+      "The romantic clash of two opinionated young people provides the sustaining theme of Pride and Prejudice. Vivacious Elizabeth Bennet is fascinated and repelled by the arrogant Mr. Darcy, whose condescending airs and acrid tongue have alienated her entire family. Their spirited courtship is conducted against a back- ground of assembly-ball flirtations and draw- ing-room intrioues.",
+    publishDate: "1961",
 
-		coverImg: 'img-1.jpeg',
-		stats: {
-			rating: 5.0,
-			reviewCount: 6,
-		},
-	},
-	{
-		id: 2,
-		title: 'Jane Eyre',
-		authors: ['Charlotte Bronte'],
-		categories: ['Fiction'],
-		description:
-			"A gothic masterpiece of tempestuous passions and dark secrets, Charlotte Bronte's Jane Eyre is edited with an introduction and notes by Stevie Davis in Penguin Classics. Charlotte Bronte tells the story of orphaned Jane Eyre, who grows up in the home of her heartless aunt, enduring loneliness and cruelty.",
-		publishDate: '2011',
-		quote:
-			'The human beings gathered round me appeared to me not realities but shadows.',
-		coverImg: 'img-2.jpeg',
-		stats: {
-			rating: 5.0,
-			reviewCount: 11,
-		},
-	},
-	{
-		id: 3,
-		title: '1984',
-		authors: ['George Orwell'],
-		categories: ['Fiction'],
-		description:
-			'1984 is a dystopian novel that was written by George Orwell and published in 1949. It tells the story of Winston Smith, a citizen of the miserable society of Oceania, who is trying to rebel against the Party and its omnipresent symbol, Big Brother.',
-		publishDate: '2013',
-		quote:
-			'Perhaps one did not want to be loved so much as to be understood.',
-		coverImg: 'img-3.jpeg',
-		stats: {
-			rating: 5.0,
-			reviewCount: 3,
-		},
-	},
-	{
-		id: 4,
-		title: 'The Great Gatsby',
-		authors: ['F. Scott Fitzgerald'],
-		categories: ['Fiction'],
-		description:
-			' Jay Gatsby, the enigmatic millionaire who throws decadent parties but doesn’t attend them, is one of the great characters of American literature. This is F. Scott Fitzgerald at his most sparkling and devastating.',
-		publishDate: '1925',
-		quote:
-			'It is a truth universally acknowledged of a good fortune, must be in want of a wife.',
-		coverImg: 'img-4.jpg',
-		stats: {
-			rating: 5.0,
-			reviewCount: 17,
-		},
-	},
-	{
-		id: 5,
-		title: ' One Hundred Years of Solitude',
-		authors: ['Gabriel García Márquez'],
-		categories: ['Fiction'],
-		description:
-			'Gabriel García Márquez’s multi-generational spanning magnum opus was a landmark in Spanish literature.',
-		publishDate: '1967',
-		quote:
-			'It is a truth universally acknowledged of a good fortune, must be in want of a wife.',
-		coverImg: 'img-5.jpg',
-		stats: {
-			rating: 5.0,
-			reviewCount: 5,
-		},
-	},
-	{
-		id: 6,
-		title: ['To Kill a Mockingbird'],
-		authors: 'Harper Lee',
-		categories: ['Fiction'],
-		description:
-			'A novel before its time, Harper Lee’s Pulitzer-prize winner addresses issues of race, inequality and segregation with both levity and compassion. Told through the eyes of loveable rogues Scout and Jem, it also created one of literature’s most beloved heroes – Atticus Finch, a man determined to right the racial wrongs of the Deep South.',
-		publishDate: '1960',
-		quote:
-			'It is a truth universally acknowledged of a good fortune, must be in want of a wife.',
-		coverImg: 'img-6.jpg',
-		stats: {
-			rating: 5.0,
-			reviewCount: 22,
-		},
-	},
-	{
-		id: 7,
-		title: ' Brave New World',
-		authors: ['Aldous Huxley'],
-		categories: ['Fiction'],
-		description:
-			'The romantic clash of two opinionated young people provides the sustaining theme of Pride and Prejudice. Vivacious Elizabeth Bennet is fascinated and repelled by the arrogant Mr. Darcy, whose condescending airs and acrid tongue have alienated her entire family. Their spirited courtship is conducted against a back- ground of assembly-ball flirtations and draw- ing-room intrioues.',
-		publishDate: '1932',
-		quote:
-			'It is a truth universally acknowledged of a good fortune, must be in want of a wife.',
-		coverImg: 'img-7.jpg',
-		stats: {
-			rating: 5.0,
-			reviewCount: 2,
-		},
-	},
+    coverImg: "img-1.jpeg",
+    stats: {
+      rating: 5.0,
+      reviewCount: 6,
+    },
+  },
+  {
+    id: 2,
+    title: "Jane Eyre",
+    authors: ["Charlotte Bronte"],
+    categories: ["Fiction"],
+    description:
+      "A gothic masterpiece of tempestuous passions and dark secrets, Charlotte Bronte's Jane Eyre is edited with an introduction and notes by Stevie Davis in Penguin Classics. Charlotte Bronte tells the story of orphaned Jane Eyre, who grows up in the home of her heartless aunt, enduring loneliness and cruelty.",
+    publishDate: "2011",
+    quote:
+      "The human beings gathered round me appeared to me not realities but shadows.",
+    coverImg: "img-2.jpeg",
+    stats: {
+      rating: 5.0,
+      reviewCount: 11,
+    },
+  },
+  {
+    id: 3,
+    title: "1984",
+    authors: ["George Orwell"],
+    categories: ["Fiction"],
+    description:
+      "1984 is a dystopian novel that was written by George Orwell and published in 1949. It tells the story of Winston Smith, a citizen of the miserable society of Oceania, who is trying to rebel against the Party and its omnipresent symbol, Big Brother.",
+    publishDate: "2013",
+    quote: "Perhaps one did not want to be loved so much as to be understood.",
+    coverImg: "img-3.jpeg",
+    stats: {
+      rating: 5.0,
+      reviewCount: 3,
+    },
+  },
+  {
+    id: 4,
+    title: "The Great Gatsby",
+    authors: ["F. Scott Fitzgerald"],
+    categories: ["Fiction"],
+    description:
+      " Jay Gatsby, the enigmatic millionaire who throws decadent parties but doesn’t attend them, is one of the great characters of American literature. This is F. Scott Fitzgerald at his most sparkling and devastating.",
+    publishDate: "1925",
+    quote:
+      "It is a truth universally acknowledged of a good fortune, must be in want of a wife.",
+    coverImg: "img-4.jpg",
+    stats: {
+      rating: 5.0,
+      reviewCount: 17,
+    },
+  },
+  {
+    id: 5,
+    title: " One Hundred Years of Solitude",
+    authors: ["Gabriel García Márquez"],
+    categories: ["Fiction"],
+    description:
+      "Gabriel García Márquez’s multi-generational spanning magnum opus was a landmark in Spanish literature.",
+    publishDate: "1967",
+    quote:
+      "It is a truth universally acknowledged of a good fortune, must be in want of a wife.",
+    coverImg: "img-5.jpg",
+    stats: {
+      rating: 5.0,
+      reviewCount: 5,
+    },
+  },
+  {
+    id: 6,
+    title: ["To Kill a Mockingbird"],
+    authors: "Harper Lee",
+    categories: ["Fiction"],
+    description:
+      "A novel before its time, Harper Lee’s Pulitzer-prize winner addresses issues of race, inequality and segregation with both levity and compassion. Told through the eyes of loveable rogues Scout and Jem, it also created one of literature’s most beloved heroes – Atticus Finch, a man determined to right the racial wrongs of the Deep South.",
+    publishDate: "1960",
+    quote:
+      "It is a truth universally acknowledged of a good fortune, must be in want of a wife.",
+    coverImg: "img-6.jpg",
+    stats: {
+      rating: 5.0,
+      reviewCount: 22,
+    },
+  },
+  {
+    id: 7,
+    title: " Brave New World",
+    authors: ["Aldous Huxley"],
+    categories: ["Fiction"],
+    description:
+      "The romantic clash of two opinionated young people provides the sustaining theme of Pride and Prejudice. Vivacious Elizabeth Bennet is fascinated and repelled by the arrogant Mr. Darcy, whose condescending airs and acrid tongue have alienated her entire family. Their spirited courtship is conducted against a back- ground of assembly-ball flirtations and draw- ing-room intrioues.",
+    publishDate: "1932",
+    quote:
+      "It is a truth universally acknowledged of a good fortune, must be in want of a wife.",
+    coverImg: "img-7.jpg",
+    stats: {
+      rating: 5.0,
+      reviewCount: 2,
+    },
+  },
+  {
+    id: 8,
+    title: "Pride and Prejustice",
+    authors: ["Jane Austen"],
+    categories: ["Fiction"],
+    description:
+      "The romantic clash of two opinionated young people provides the sustaining theme of Pride and Prejudice. Vivacious Elizabeth Bennet is fascinated and repelled by the arrogant Mr. Darcy, whose condescending airs and acrid tongue have alienated her entire family. Their spirited courtship is conducted against a back- ground of assembly-ball flirtations and draw- ing-room intrioues.",
+    publishDate: "1961",
+
+    coverImg: "img-1.jpeg",
+    stats: {
+      rating: 5.0,
+      reviewCount: 6,
+    },
+  },
 ];
