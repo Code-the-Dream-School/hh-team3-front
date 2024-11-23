83843ea8efad79a1ba3d445b9194bf5e9e3c8e40
olgakahraman <ol.khrmn@gmail.com>
2024-11-22 19:12:12 -0500

    created comments folder

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -5,6 +5,7 @@ import Loader from "../Loader/Loader.jsx";
 import "./BookDetails.css";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import BookTalks from '../BookTalks/BookTalks.jsx'
 
 export default function BookDetails() {
 	const { id } = useParams();
@@ -79,6 +80,7 @@ export default function BookDetails() {
 					</div>
 				</div>
 			</div>
+			<BookTalks />
 		</div>
 	);
 }
