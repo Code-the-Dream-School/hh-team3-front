761cada66a42259ca31c9fd11e92b0ddf6a847f8
betymesfin <98676637+betymesfin@users.noreply.github.com>
2024-12-02 18:07:16 -0600

    Home (#7)
    * combined bookcard and nav bar
    
    * added home page
    
    * Home page added with Book of the Month
    
    * added a Findbook page separatly
    
    ---------
    
    Co-authored-by: olgakahraman <117988074+olgakahraman@users.noreply.github.com>

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,22 +1,22 @@
 import React from "react";
 import "./BookCard.css";
 
-export default function BookCard({
-	coverImg,
-	authors = [],
-	title,
-	publishDate,
-}) {
-	return (
-		<div className="card day-theme">
-			<img src={`../images/${coverImg}`} />
-			<div className="card-content">
-				<p className="card-title"> {title}</p>
-				<p className="book-author">
-					By {authors.join(', ')} {publishDate}
-				</p>
-			</div>
-		</div>
-	);
 
+export default function BookCard({
+  imageLinks = {},
+  authors = [],
+  title,
+  publishedDate,
+}) {
+  return (
+    <div className="card day-theme">
+      <img src={`../images/${imageLinks.thumbnail}`} alt="Book cover" />
+      <div className="card-content">
+        <p className="card-title"> {title}</p>
+        <p className="card-author">
+          By {authors.join(",")} ({publishedDate})
+        </p>
+      </div>
+    </div>
+  );
 }
