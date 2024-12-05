a618103302462b90e62606438f707cfda97e5209
betymesfin <98676637+betymesfin@users.noreply.github.com>
2024-12-05 17:53:38 -0600

    modification

--- src/components/BookDetails/BookDetails.jsx
+++ src/components/BookDetails/BookDetails.jsx
@@ -3,22 +3,19 @@ import BookTalks from "../BookTalks/BookTalks.jsx";
 import "./BookDetails.css";
 
 export default function BookDetails({
-	imageLinks = {},
-	authors = [],
-	title,
-	publishedDate,
-	description,
-	categories = [],
+  imageLinks = {},
+  authors = [],
+  title,
+  publishedDate,
+  description,
+  categories = [],
 }) {
-	return (
-		<div className="book-details-page">
-			<div className="book-details-container day-theme">
-				<img
-					src={`../images/${imageLinks.thumbnail}`}
-					alt="Thumbnail"
-				/>
-				<div className="book-details">
-					<p className="book-title">{title}</p>
+  return (
+    <div className="book-details-page">
+      <div className="book-details-container day-theme">
+        <img src={`../images/${imageLinks.thumbnail}`} alt="Thumbnail" />
+        <div className="book-details">
+          <p className="book-title">{title}</p>
 
 					<p className="book-author">
 						By {authors.join(", ")} ({publishedDate})
