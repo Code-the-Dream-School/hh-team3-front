900be4a75468866ccfd5079f943538d4aac90954
olgakahraman <ol.khrmn@gmail.com>
2024-12-07 21:05:12 -0500

    improved day/night theme feature
    Added CSS variables for day-night theme in App.css and applied them across components. Cleaned up unused classNames, added smooth transitions for the entire app, and set global * { margin: 0; padding: 0 } for consistency.

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import Navbar from "./components/NavBar/Navbar";
 import booksData from "/src/data/booksData.js";
 import Home from "./Pages/home";
 import FindABook from "./Pages/FindABook";
+import './App.css'
+const URL = "http://localhost:8000/api/v1/";
 
 function App() {
-	return (
-		<>
-			<BrowserRouter>
-				<Navbar />
-				<Routes>
-					<Route path="/find-book" element={<FindABook />} />
-					<Route path="/" element={<Home booksData={booksData} />} />
-				</Routes>
-			</BrowserRouter>
-		</>
-	);
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+
+          <Route path="/find-book" element={<FindABook />} />
+          <Route path="/" element={<Home booksData={booksData} />} />
+
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 export default App;
