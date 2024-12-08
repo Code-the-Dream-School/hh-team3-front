bfe3d89e843dce79d5429d44c0b4c612456091d3
Elijah Smith <mcditto99@gmail.com>
2024-12-08 17:25:29 -0600

    Fix dummy book data
    - Set `publishDate` to `publishedDate` everywhere, as that is the field our code references.
    - Remove unused properties.
    - Convert `coverImg` to `imageLinks` object for all remaining objects.

--- src/data/booksData.js
+++ src/data/booksData.js
@@ -1,116 +1,106 @@
 export default [
 	{
 		id: 1,
-		title: 'Pride and Prejustice',
-		googleID: ' ',
-		authors: ['Jane Austen'],
-		publisher: ' ',
+		title: "Pride and Prejustice",
+		authors: ["Jane Austen"],
 		description:
-			'The romantic clash of two opinionated young people provides the sustaining theme of Pride and Prejudice. Vivacious Elizabeth Bennet is fascinated and repelled by the arrogant Mr. Darcy, whose condescending airs and acrid tongue have alienated her entire family. Their spirited courtship is conducted against a back- ground of assembly-ball flirtations and draw- ing-room intrioues.',
-		categories: ['Fiction', 'Non-fiction'],
-		publishedDate: '1961',
+			"The romantic clash of two opinionated young people provides the sustaining theme of Pride and Prejudice. Vivacious Elizabeth Bennet is fascinated and repelled by the arrogant Mr. Darcy, whose condescending airs and acrid tongue have alienated her entire family. Their spirited courtship is conducted against a back- ground of assembly-ball flirtations and draw- ing-room intrioues.",
+		categories: ["Fiction", "Non-fiction"],
+		publishedDate: "1961",
 		quote:
-			'The human beings gathered round me appeared to me not realities but shadows.',
+			"The human beings gathered round me appeared to me not realities but shadows.",
 		imageLinks: {
-			smallThumbnail: '',
-			thumbnail: 'img-1.jpeg',
+			smallThumbnail: "",
+			thumbnail: "img-1.jpeg",
 		},
 	},
 	{
 		id: 2,
-		title: 'Jane Eyre',
-		authors: ['Charlotte Bronte', 'Lermontov'],
-		categories: ['Fiction', 'Non-fiction'],
+		title: "Jane Eyre",
+		authors: ["Charlotte Bronte", "Lermontov"],
+		categories: ["Fiction", "Non-fiction"],
 		description:
 			"A gothic masterpiece of tempestuous passions and dark secrets, Charlotte Bronte's Jane Eyre is edited with an introduction and notes by Stevie Davis in Penguin Classics. Charlotte Bronte tells the story of orphaned Jane Eyre, who grows up in the home of her heartless aunt, enduring loneliness and cruelty.",
-		publishDate: '2011',
+		publishedDate: "2011",
 		quote:
-			'The human beings gathered round me appeared to me not realities but shadows.',
-		coverImg: 'img-2.jpeg',
-		stats: {
-			rating: 5.0,
-			reviewCount: 11,
+			"The human beings gathered round me appeared to me not realities but shadows.",
+		imageLinks: {
+			smallThumbnail: "",
+			thumbnail: "img-2.jpeg",
 		},
 	},
 	{
 		id: 3,
-		title: '1984',
-		authors: ['George Orwell'],
-		categories: ['Fiction'],
+		title: "1984",
+		authors: ["George Orwell"],
+		categories: ["Fiction"],
 		description:
-			'1984 is a dystopian novel that was written by George Orwell and published in 1949. It tells the story of Winston Smith, a citizen of the miserable society of Oceania, who is trying to rebel against the Party and its omnipresent symbol, Big Brother.',
-		publishDate: '2013',
-		quote:
-			'Perhaps one did not want to be loved so much as to be understood.',
-		coverImg: 'img-3.jpeg',
-		stats: {
-			rating: 5.0,
-			reviewCount: 3,
+			"1984 is a dystopian novel that was written by George Orwell and published in 1949. It tells the story of Winston Smith, a citizen of the miserable society of Oceania, who is trying to rebel against the Party and its omnipresent symbol, Big Brother.",
+		publishedDate: "2013",
+		quote: "Perhaps one did not want to be loved so much as to be understood.",
+		imageLinks: {
+			smallThumbnail: "",
+			thumbnail: "img-3.jpeg",
 		},
 	},
 	{
 		id: 4,
-		title: 'The Great Gatsby',
-		authors: ['F. Scott Fitzgerald'],
-		categories: ['Fiction'],
+		title: "The Great Gatsby",
+		authors: ["F. Scott Fitzgerald"],
+		categories: ["Fiction"],
 		description:
 			"Jay Gatsby, the enigmatic millionaire who throws decadent parties but doesn't attend them, is one of the great characters of American literature. This is F. Scott Fitzgerald at his most sparkling and devastating.",
-		categories: ['Fiction'],
-		publishedDate: '1925',
+		categories: ["Fiction"],
+		publishedDate: "1925",
 		quote:
-			'It is a truth universally acknowledged of a good fortune, must be in want of a wife.',
+			"It is a truth universally acknowledged of a good fortune, must be in want of a wife.",
 		imageLinks: {
-			smallThumbnail: '',
-			thumbnail: 'img-4.jpg',
+			smallThumbnail: "",
+			thumbnail: "img-4.jpg",
 		},
 	},
 	{
 		id: 5,
-		title: ' One Hundred Years of Solitude',
-		authors: ['Gabriel García Márquez'],
-		categories: ['Fiction'],
+		title: "One Hundred Years of Solitude",
+		authors: ["Gabriel García Márquez"],
+		categories: ["Fiction"],
 		description:
 			"Gabriel García Márquez's multi-generational spanning magnum opus was a landmark in Spanish literature",
-		categories: ['Fiction'],
-		publishedDate: '1967',
-		quote:
-			'The human beings gathered round me appeared to me not realities but shadows.',
+		categories: ["Fiction"],
+		publishedDate: "1967",
+		quote: "Death really did not matter to him but life did, and therefore the sensation he felt when they gave their decision was not a feeling of fear but of nostalgia.",
 		imageLinks: {
-			smallThumbnail: '',
-			thumbnail: 'img-5.jpg',
+			smallThumbnail: "",
+			thumbnail: "img-5.jpg",
 		},
 	},
 	{
 		id: 6,
-		title: 'To Kill a Mockingbird',
-		authors: ['Harper Lee'],
-		categories: ['Fiction'],
+		title: "To Kill a Mockingbird",
+		authors: ["Harper Lee"],
+		categories: ["Fiction"],
 		description:
 			"A novel before its time, Harper Lee's Pulitzer-prize winner addresses issues of race, inequality and segregation with both levity and compassion. Told through the eyes of loveable rogues Scout and Jem, it also created one of literature's most beloved heroes – Atticus Finch, a man determined to right the racial wrongs of the Deep South.",
-		categories: ['Fiction'],
-		publishedDate: '1960',
-		quote:
-			'The human beings gathered round me appeared to me not realities but shadows.',
+		categories: ["Fiction"],
+		publishedDate: "1960",
+		quote: "You never really understand a person until you consider things from his point of view... Until you climb inside of his skin and walk around in it.",
 		imageLinks: {
-			smallThumbnail: '',
-			thumbnail: 'img-6.jpg',
+			smallThumbnail: "",
+			thumbnail: "img-6.jpg",
 		},
 	},
 	{
 		id: 7,
-		title: 'Brave New World',
-		googleID: ' ',
-		authors: ['Aldous Huxley'],
-		categories: ['Fiction'],
+		title: "Brave New World",
+		authors: ["Aldous Huxley"],
+		categories: ["Fiction"],
 		description:
-			'The romantic clash of two opinionated young people provides the sustaining theme of Pride and Prejudice. Vivacious Elizabeth Bennet is fascinated and repelled by the arrogant Mr. Darcy, whose condescending airs and acrid tongue have alienated her entire family. Their spirited courtship is conducted against a back- ground of assembly-ball flirtations and draw- ing-room intrioues.',
-		publishDate: '1932',
-		quote:
-			'The human beings gathered round me appeared to me not realities but shadows.',
+			"Brave New World is a dystopian novel by English author Aldous Huxley, written in 1931 and published in 1932.[3] Largely set in a futuristic World State, whose citizens are environmentally engineered into an intelligence-based social hierarchy, the novel anticipates huge scientific advancements in reproductive technology, sleep-learning, psychological manipulation and classical conditioning that are combined to make a dystopian society which is challenged by the story's protagonist. Huxley followed this book with a reassessment in essay form, Brave New World Revisited (1958), and with his final novel, Island (1962), the utopian counterpart. This novel is often compared as an inversion counterpart to George Orwell's 1984 (1949).",
+		publishedDate: "1932",
+		quote: "Words can be like X-rays if you use them properly – they’ll go through anything. You read and you’re pierced.",
 		imageLinks: {
-			smallThumbnail: '',
-			thumbnail: 'img-7.jpg',
+			smallThumbnail: "",
+			thumbnail: "img-7.jpg",
 		},
 	},
-
 ];
