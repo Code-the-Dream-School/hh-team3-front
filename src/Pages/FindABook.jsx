4572ae83e73ba0d95b883691567772f422f9b3ab
Elijah Smith <Elijah.Smith@imc.com>
2024-12-09 18:36:51 -0600

    Search bar component

--- src/Pages/FindABook.jsx
+++ src/Pages/FindABook.jsx
@@ -1,16 +1,34 @@
-import React from "react";
-import booksData from "../data/booksData";
-import BookCard from "../components/BookCard/BookCard";
 
-function FindABook({}) {
-	const cards = booksData.map((item) => {
-		return <BookCard key={item.id} {...item} />;
-	});
-	return (
-		<>
-			<div className="day-theme">{cards}</div>
-		</>
-	);
+import React, { useState } from "react";
+import booksData from "../data/booksData";
+import Search from "../components/Search/search";
+import SearchList from "../components/Search/searchList";
+
+function FindABook() {
+  const [filteredData, setFilteredData] = useState(booksData)
+
+  const handleSearch = (query) => {
+    const lowercasedQuery = query.toLowerCase();
+    const filtered = booksData.filter(
+      (book) =>
+        book.title.toLowerCase().includes(lowercasedQuery) ||
+        book.authors.some((author) =>
+          author.toLowerCase().includes(lowercasedQuery),
+        ) ||
+        book.categories.some((category) =>
+          category.toLowerCase().includes(lowercasedQuery),
+        ),
+    );
+    setFilteredData(filtered);
+  };
+
+  return (
+    <>
+      <Search onSearch={handleSearch} />
+      <SearchList filteredData={filteredData} />
+    </>
+  );
+
 }
 
 export default FindABook;
