edf399108971f0c1068612b73860d2994baf076e
betymesfin <98676637+betymesfin@users.noreply.github.com>
2024-12-11 10:44:15 -0600

    api added to the bookdetail and find a book

--- src/components/BookCard/BookCard.jsx
+++ src/components/BookCard/BookCard.jsx
@@ -1,23 +1,22 @@
-import React from 'react';
-import './BookCard.css';
+import React from "react";
+import "./BookCard.css";
 
 export default function BookCard({
 	imageLinks = {},
 	authors = [],
 	title,
 	publishedDate,
+	children,
 }) {
 	return (
 		<div className="card">
-			<img
-				src={`../images/${imageLinks.thumbnail}`}
-				alt="Book cover"
-			/>
+			<img src={imageLinks?.thumbnail} alt="Book cover" />
 			<div className="card-content">
 				<p className="card-title">{title}</p>
 				<p className="card-author">
-					By {authors.join(', ')} ({publishedDate})
+					By {authors.join(",")} ({publishedDate.split("-")[0]})
 				</p>
+				{children}
 			</div>
 		</div>
 	);
