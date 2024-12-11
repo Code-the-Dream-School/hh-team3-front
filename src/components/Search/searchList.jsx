9cff940edcf2730886a82e6fc9e66e727436f1fc
betymesfin <98676637+betymesfin@users.noreply.github.com>
2024-12-11 10:44:15 -0600

    api added to the bookdetail and find a book

--- src/components/Search/searchList.jsx
+++ src/components/Search/searchList.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import BookCard from "../BookCard/BookCard";
 import "./search.css";
+import { Link } from "react-router-dom";
 
 function SearchList({ filteredData }) {
-  return <div className="display">{filteredData.map((book) => (
-    <BookCard key={book.id} {...book} />
-  ))}</div>;
+	return (
+		<div className="display">
+			{filteredData.map((book) => (
+				<BookCard key={book.id} {...book}>
+					<Link to={`/books/${book.id}`}>
+						<button>View Details</button>
+					</Link>
+				</BookCard>
+			))}
+		</div>
+	);
 }
 
 export default SearchList;
--- src/components/day-night-theme/DayNightTheme.jsx
+++ src/components/day-night-theme/DayNightTheme.jsx
@@ -1,34 +1,24 @@
-import React from 'react';
-import { useColorScheme } from '../../hooks/useColorScheme';
-import '../BookCard/BookCard.css';
-import '../BookDetails/BookDetails.css';
-import './DayNightTheme.css';
+import React from "react";
+import { useColorScheme } from "../../hooks/useColorScheme";
+import "../BookCard/BookCard.css";
+import "../BookDetails/BookDetails.css";
+import "./DayNightTheme.css";
 
 export default function DayNightTheme() {
 	const { isDark, toggleTheme } = useColorScheme();
 
 	return (
 		<div className="toggle">
-			<p
-				className={`toggle-text ${
-					isDark ? 'unselected' : ''
-				}`}
-			>
+			<p className={`toggle-text ${isDark ? "unselected" : ""}`}>
 				<i className="fa-regular fa-sun"></i>
 			</p>
 			<div
-				className={`toggle-button ${
-					isDark ? 'toggled' : ''
-				}`}
+				className={`toggle-button ${isDark ? "toggled" : ""}`}
 				onClick={toggleTheme}
 			>
 				<div className="inner-circle"></div>
 			</div>
-			<p
-				className={`toggle-text ${
-					!isDark ? 'unselected' : ''
-				}`}
-			>
+			<p className={`toggle-text ${!isDark ? "unselected" : ""}`}>
 				<i className="fa-solid fa-moon"></i>
 			</p>
 		</div>
