7cad4c914c0dddae9eff21ed83102ebdc3de0077
olgakahraman <ol.khrmn@gmail.com>
2024-12-12 18:53:29 -0500

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import "./App.css";
 import Navbar from "./components/NavBar/Navbar";
 import FindABook from "./Pages/FindABook";
 import Home from "./Pages/home";
 import booksData from "/src/data/booksData.js";
+import "./App.css";
 
 const URL = "http://localhost:8000/api/v1/";
 
