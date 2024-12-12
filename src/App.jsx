c8f6068a8a79cf60d0991ea5034461f21f474770
Bakary Sylla <syllabakary2002@gmail.com>
2024-12-12 17:06:05 -0600

    login component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,14 @@
-import {
-	BrowserRouter,
-	Route,
-	Routes,
-} from 'react-router-dom';
-import './App.css';
-import Navbar from './components/NavBar/Navbar';
-import FindABook from './Pages/FindABook';
-import Home from './Pages/home';
-import booksData from '/src/data/booksData.js';
-const URL = 'http://localhost:8000/api/v1/';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Navbar from "./components/NavBar/Navbar";
+import FindABook from "./Pages/FindABook";
+import Home from "./Pages/home";
+import booksData from "/src/data/booksData.js";
+import Login from "./components/UserLogin/Login";
+const URL = "http://localhost:8000/api/v1/";
 
 function App() {
-	if (typeof global === 'undefined') {
+	if (typeof global === "undefined") {
 		window.global = window;
 	}
 	return (
@@ -19,14 +16,9 @@ function App() {
 			<BrowserRouter>
 				<Navbar />
 				<Routes>
-					<Route
-						path="/find-book"
-						element={<FindABook />}
-					/>
-					<Route
-						path="/"
-						element={<Home booksData={booksData} />}
-					/>
+					<Route path="/find-book" element={<FindABook />} />
+					<Route path="/" element={<Home booksData={booksData} />} />
+					<Route path="/login" element={<Login />} />
 				</Routes>
 			</BrowserRouter>
 		</>
