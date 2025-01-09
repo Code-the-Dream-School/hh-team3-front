e6ebff8a6960405bd6befefe180c94dd9268d7eb
betymesfin <98676637+betymesfin@users.noreply.github.com>
2025-01-09 00:43:09 -0600

    DiscussionList

---
+++ src/Pages/FIndADiscussion.jsx
@@ -0,0 +1,35 @@
+import React, { useState, useEffect } from "react";
+import Search from "../components/Search/search.jsx";
+import DiscussionList from "../components/Discussion/DiscussionList/DiscussionList";
+
+function FindADiscussion({ discussionsData }) {
+	console.log(discussionsData);
+	const [filteredData, setFilteredData] = useState(discussionsData || []);
+	useEffect(() => {
+		setFilteredData(discussionsData);
+	}, [discussionsData]);
+
+	const handleSearch = (query) => {
+		const lowercasedQuery = query.toLowerCase();
+		const filtered = discussionsData.filter((discussion) => {
+			const formattedDate = new Date(
+				discussion.date,
+			).toLocaleDateString();
+			return (
+				discussion.title.toLowerCase().includes(lowercasedQuery) ||
+				discussion.book.toLowerCase().includes(lowercasedQuery) ||
+				formattedDate.includes(lowercasedQuery)
+			);
+		});
+		setFilteredData(filtered);
+	};
+
+	return (
+		<>
+			<Search onSearch={handleSearch} />
+			<DiscussionList filteredData={filteredData} />
+		</>
+	);
+}
+
+export default FindADiscussion;
