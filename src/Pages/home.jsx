b7cd5e499a8c41283af437d85c1a55171af6b32f
olgakahraman <ol.khrmn@gmail.com>
2025-01-16 20:44:20 -0500

    added up-page link for discussionList, improved userPage css

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -35,72 +35,66 @@ function Home({ booksData }) {
 
 	return (
 		<div className="home">
-			<main className="d-flex flex-column-reverse flex-md-row justify-content-between align-items-center">
-				<section className="welcome">
-					<h1 className="mb-4">Welcome to BookTalk</h1>
-					<h4 className="mb-3 fst-italic fw-bolder">
-						Dive Into a World of Books and Conversations!
-					</h4>
-					<p>
-						Our platform brings together book lovers from around the
-						world, giving you the perfect space to share your
-						passion for reading. Whether you're looking to discuss
-						your latest read or discover new books through others'
-						perspectives, we offer a unique experience where you can
-						create or join engaging book discussions.
-					</p>
-					<p>
-						<strong className="fst-italic">Create your own discussions</strong> and invite
-						fellow readers to dive deep into the themes, characters,
-						and plots that intrigue you most.
-					</p>
-					<p>
-						<strong className="fst-italic">Join ongoing conversations</strong> to share
-						your thoughts, ask questions, and connect with
-						like-minded individuals who share your love for
-						literature.
-					</p>
-					<p>
-						Whether you're a fan of fiction, non-fiction, fantasy,
-						or mystery, there's a place for you here.
-					</p>
-					<p>
-						<strong className="fst-italic">Connect with new friends</strong>, exchange
-						recommendations, and explore books in ways you never
-						have before.
-					</p>
-					<h4 className="mt-4 fst-italic fw-bolder">
-						Start talking, start reading, start connecting — all in
-						one place.
-					</h4>
-				</section>
-
-				<section className="book-of-the-month">
-					<h4 className="fst-italic fw-bolder">What&apos;s Behind the Pages?</h4>
-					<h4 className="mb-4 fst-italic fw-bolder">Let&apos;s Discuss!</h4>
-					<Link
-						className="book-of-the-month-card"
-						style={{ textDecoration: "none" }}
-						to={`/books/${randomBook.id || randomBook._id}`}
-					>
-						<img
-							src={randomBook.imageLinks?.thumbnail}
-							alt={randomBook.title}
-							className="book-of-the-month-card-img"
-						/>
-						<div>
-							<h5 className="book-of-the-month-card-title">
-								{randomBook.title}
-							</h5>
-							<p className="book-of-the-month-card-author">
-								By {randomBook.authors.join(", ")} (
-								{randomBook.publishedDate.split("-")[0]})
-							</p>
-						</div>
-					</Link>
-				</section>
-			</main>
-
+	<main className="d-flex flex-column flex-md-row justify-content-between align-items-center">
+    <section className="welcome order-md-0 order-1">
+        <h1 className="mb-4">Welcome to BookTalk</h1>
+        <h4 className="mb-3 fst-italic fw-bolder">
+            Dive Into a World of Books and Conversations!
+        </h4>
+        <p>
+            Our platform brings together book lovers from around the world,
+            giving you the perfect space to share your passion for reading.
+            Whether you're looking to discuss your latest read or discover
+            new books through others' perspectives, we offer a unique experience
+            where you can create or join engaging book discussions.
+        </p>
+        <p>
+            <strong className="fst-italic">Create your own discussions</strong> and invite
+            fellow readers to dive deep into the themes, characters, and plots
+            that intrigue you most.
+        </p>
+        <p>
+            <strong className="fst-italic">Join ongoing conversations</strong> to share
+            your thoughts, ask questions, and connect with like-minded
+            individuals who share your love for literature.
+        </p>
+        <p>
+            Whether you're a fan of fiction, non-fiction, fantasy, or mystery,
+            there's a place for you here.
+        </p>
+        <p>
+            <strong className="fst-italic">Connect with new friends</strong>, exchange
+            recommendations, and explore books in ways you never have before.
+        </p>
+        <h4 className="mt-4 fst-italic fw-bolder">
+            Start talking, start reading, start connecting — all in one place.
+        </h4>
+    </section>
+		 <section className="book-of-the-month order-md-1 order-0">
+        <h4 className="fst-italic fw-bolder">What&apos;s Behind the Pages?</h4>
+        <h4 className="mb-4 fst-italic fw-bolder">Let&apos;s Discuss!</h4>
+        <Link
+            className="book-of-the-month-card"
+            style={{ textDecoration: "none" }}
+            to={`/books/${randomBook.id || randomBook._id}`}
+        >
+            <img
+                src={randomBook.imageLinks?.thumbnail}
+                alt={randomBook.title}
+                className="book-of-the-month-card-img"
+            />
+            <div>
+                <h5 className="book-of-the-month-card-title">
+                    {randomBook.title}
+                </h5>
+                <p className="book-of-the-month-card-author">
+                    By {randomBook.authors.join(", ")} (
+                    {randomBook.publishedDate.split("-")[0]})
+                </p>
+            </div>
+        </Link>
+    </section>
+</main>
 			<section className="links d-flex flex-wrap justify-content-center gap-4 my-5">
 				<Link
 					className="btn btn-outline-light book-btn d-flex flex-column align-items-center p-4 shadow"
